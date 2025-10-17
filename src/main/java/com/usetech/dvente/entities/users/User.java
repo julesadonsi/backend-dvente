137802dc2f98dd10274d4a4a8653098632167961
julesadonsi <adonsijules9@gmail.com>
2025-10-17 17:35:07 +0100

    fix

--- src/main/java/com/usetech/dvente/entities/users/User.java
+++ src/main/java/com/usetech/dvente/entities/users/User.java
@@ -8,7 +8,6 @@ import org.hibernate.annotations.UpdateTimestamp;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.web.client.RestTemplate;
 
 import java.time.LocalDateTime;
 import java.util.Collection;
